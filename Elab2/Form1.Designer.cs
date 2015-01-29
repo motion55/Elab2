fa8259e7acae4f42e948570383d6223471c01adf
butchtabios <butchtabios@live.com>
2015-01-29 14:23:14 +0800

    new version from home

--- Elab2/Form1.Designer.cs
+++ Elab2/Form1.Designer.cs
@@ -28,34 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.AnalogReadButton = new System.Windows.Forms.Button();
-            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.WButton = new System.Windows.Forms.Button();
             this.ConnectButton = new System.Windows.Forms.Button();
-            this.LEDcheckBox = new System.Windows.Forms.CheckBox();
-            this.AnalogVal = new System.Windows.Forms.TextBox();
+            this.IPAddress = new System.Windows.Forms.TextBox();
+            this.SButton = new System.Windows.Forms.Button();
+            this.ReceivedTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // AnalogReadButton
+            // WButton
             // 
-            this.AnalogReadButton.Location = new System.Drawing.Point(183, 137);
-            this.AnalogReadButton.Name = "AnalogReadButton";
-            this.AnalogReadButton.Size = new System.Drawing.Size(108, 36);
-            this.AnalogReadButton.TabIndex = 0;
-            this.AnalogReadButton.Text = "Analog Read";
-            this.AnalogReadButton.UseVisualStyleBackColor = true;
-            this.AnalogReadButton.Click += new System.EventHandler(this.AnalogRead_Click);
-            // 
-            // PortComboBox
-            // 
-            this.PortComboBox.FormattingEnabled = true;
-            this.PortComboBox.Location = new System.Drawing.Point(27, 45);
-            this.PortComboBox.Name = "PortComboBox";
-            this.PortComboBox.Size = new System.Drawing.Size(114, 24);
-            this.PortComboBox.TabIndex = 2;
+            this.WButton.Location = new System.Drawing.Point(96, 115);
+            this.WButton.Name = "WButton";
+            this.WButton.Size = new System.Drawing.Size(83, 36);
+            this.WButton.TabIndex = 0;
+            this.WButton.Text = "W";
+            this.WButton.UseVisualStyleBackColor = true;
+            this.WButton.Click += new System.EventHandler(this.W_Click);
             // 
             // ConnectButton
             // 
-            this.ConnectButton.Location = new System.Drawing.Point(183, 38);
+            this.ConnectButton.Location = new System.Drawing.Point(265, 38);
             this.ConnectButton.Name = "ConnectButton";
             this.ConnectButton.Size = new System.Drawing.Size(108, 36);
             this.ConnectButton.TabIndex = 3;
@@ -63,36 +55,44 @@
             this.ConnectButton.UseVisualStyleBackColor = true;
             this.ConnectButton.Click += new System.EventHandler(this.Connect_Click);
             // 
-            // LEDcheckBox
+            // IPAddress
             // 
-            this.LEDcheckBox.AutoSize = true;
-            this.LEDcheckBox.Location = new System.Drawing.Point(343, 48);
-            this.LEDcheckBox.Name = "LEDcheckBox";
-            this.LEDcheckBox.Size = new System.Drawing.Size(57, 21);
-            this.LEDcheckBox.TabIndex = 4;
-            this.LEDcheckBox.Text = "LED";
-            this.LEDcheckBox.UseVisualStyleBackColor = true;
-            this.LEDcheckBox.CheckedChanged += new System.EventHandler(this.LEDcheckBox_CheckedChanged);
+            this.IPAddress.Location = new System.Drawing.Point(96, 45);
+            this.IPAddress.Name = "IPAddress";
+            this.IPAddress.ReadOnly = true;
+            this.IPAddress.Size = new System.Drawing.Size(148, 22);
+            this.IPAddress.TabIndex = 5;
+            this.IPAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // AnalogVal
+            // SButton
             // 
-            this.AnalogVal.Location = new System.Drawing.Point(27, 144);
-            this.AnalogVal.Name = "AnalogVal";
-            this.AnalogVal.ReadOnly = true;
-            this.AnalogVal.Size = new System.Drawing.Size(114, 22);
-            this.AnalogVal.TabIndex = 5;
-            this.AnalogVal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.SButton.Location = new System.Drawing.Point(290, 115);
+            this.SButton.Name = "SButton";
+            this.SButton.Size = new System.Drawing.Size(83, 36);
+            this.SButton.TabIndex = 6;
+            this.SButton.Text = "S";
+            this.SButton.UseVisualStyleBackColor = true;
+            this.SButton.Click += new System.EventHandler(this.S_Click);
+            // 
+            // ReceivedTextBox
+            // 
+            this.ReceivedTextBox.Location = new System.Drawing.Point(96, 184);
+            this.ReceivedTextBox.Name = "ReceivedTextBox";
+            this.ReceivedTextBox.ReadOnly = true;
+            this.ReceivedTextBox.Size = new System.Drawing.Size(277, 22);
+            this.ReceivedTextBox.TabIndex = 7;
+            this.ReceivedTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 243);
-            this.Controls.Add(this.AnalogVal);
-            this.Controls.Add(this.LEDcheckBox);
+            this.Controls.Add(this.ReceivedTextBox);
+            this.Controls.Add(this.SButton);
+            this.Controls.Add(this.IPAddress);
             this.Controls.Add(this.ConnectButton);
-            this.Controls.Add(this.PortComboBox);
-            this.Controls.Add(this.AnalogReadButton);
+            this.Controls.Add(this.WButton);
             this.Name = "Form1";
             this.Text = "ArduinoSocketTest";
             this.ResumeLayout(false);
@@ -102,11 +102,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button AnalogReadButton;
-        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.Button WButton;
         private System.Windows.Forms.Button ConnectButton;
-        private System.Windows.Forms.CheckBox LEDcheckBox;
-        private System.Windows.Forms.TextBox AnalogVal;
+        private System.Windows.Forms.TextBox IPAddress;
+        private System.Windows.Forms.Button SButton;
+        private System.Windows.Forms.TextBox ReceivedTextBox;
     }
 }
 
--- Elab2/Form1.cs
+++ Elab2/Form1.cs
@@ -14,276 +14,57 @@ using System.Net.Sockets;
 
 namespace Elab
 {
-    enum Verbs
-    {
-        WILL = 251,
-        WONT = 252,
-        DO = 253,
-        DONT = 254,
-        IAC = 255
-    }
-
-    enum Options
-    {
-        SGA = 3
-    }
-
-    class TelnetConnection
-    {
-        TcpClient tcpSocket;
-
-        int TimeOutMs = 100;
-
-        public TelnetConnection(string Hostname, int Port)
-        {
-            tcpSocket = new TcpClient(Hostname, Port);
-
-        }
-
-        public string Login(string Username, string Password, int LoginTimeOutMs)
-        {
-            int oldTimeOutMs = TimeOutMs;
-            TimeOutMs = LoginTimeOutMs;
-            string s = Read();
-            if (!s.TrimEnd().EndsWith(":"))
-                throw new Exception("Failed to connect : no login prompt");
-            WriteLine(Username);
-
-            s += Read();
-            if (!s.TrimEnd().EndsWith(":"))
-                throw new Exception("Failed to connect : no password prompt");
-            WriteLine(Password);
-
-            s += Read();
-            TimeOutMs = oldTimeOutMs;
-            return s;
-        }
-
-        public void WriteLine(string cmd)
-        {
-            Write(cmd + "\n");
-        }
-
-        public void Write(string cmd)
-        {
-            if (!tcpSocket.Connected) return;
-            byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd.Replace("\0xFF", "\0xFF\0xFF"));
-            tcpSocket.GetStream().Write(buf, 0, buf.Length);
-        }
-
-        public string Read()
-        {
-            if (!tcpSocket.Connected) return null;
-            StringBuilder sb = new StringBuilder();
-            do
-            {
-                ParseTelnet(sb);
-                System.Threading.Thread.Sleep(TimeOutMs);
-            } while (tcpSocket.Available > 0);
-            return sb.ToString();
-        }
-
-        public bool IsConnected
-        {
-            get { return tcpSocket.Connected; }
-        }
-
-        void ParseTelnet(StringBuilder sb)
-        {
-            while (tcpSocket.Available > 0)
-            {
-                int input = tcpSocket.GetStream().ReadByte();
-                switch (input)
-                {
-                    case -1:
-                        break;
-                    case (int)Verbs.IAC:
-                        // interpret as command
-                        int inputverb = tcpSocket.GetStream().ReadByte();
-                        if (inputverb == -1) break;
-                        switch (inputverb)
-                        {
-                            case (int)Verbs.IAC:
-                                //literal IAC = 255 escaped, so append char 255 to string
-                                sb.Append(inputverb);
-                                break;
-                            case (int)Verbs.DO:
-                            case (int)Verbs.DONT:
-                            case (int)Verbs.WILL:
-                            case (int)Verbs.WONT:
-                                // reply to all commands with "WONT", unless it is SGA (suppres go ahead)
-                                int inputoption = tcpSocket.GetStream().ReadByte();
-                                if (inputoption == -1) break;
-                                tcpSocket.GetStream().WriteByte((byte)Verbs.IAC);
-                                if (inputoption == (int)Options.SGA)
-                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WILL : (byte)Verbs.DO);
-                                else
-                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WONT : (byte)Verbs.DONT);
-                                tcpSocket.GetStream().WriteByte((byte)inputoption);
-                                break;
-                            default:
-                                break;
-                        }
-                        break;
-                    default:
-                        sb.Append((char)input);
-                        break;
-                }
-            }
-        }
-    }
-
     public partial class Form1 : Form
     {
-        Socket _socket = null;
+        TcpClient tcpSocket = null;
 
         public Form1()
         {
             InitializeComponent();
-
-            GetSerialPorts();
-        }
-
-        private void GetSerialPorts()
-        {
-            string[] ArrayComPortsNames = null;
-            int index = -1;
-            string ComPortName = null;
-
-            ArrayComPortsNames = SerialPort.GetPortNames();
-
-            do {
-                index += 1;
-                PortComboBox.Items.Add(ArrayComPortsNames[index]);
-            } while (!((ArrayComPortsNames[index] == ComPortName) ||
-                       (index == ArrayComPortsNames.GetUpperBound(0))));
-
-            Array.Sort(ArrayComPortsNames);
-
-            //want to get first out
-            if (index == ArrayComPortsNames.GetUpperBound(0))
-            {
-                ComPortName = ArrayComPortsNames[0];
-            }
-            PortComboBox.Text = ArrayComPortsNames[0];
         }
 
         private void Connect_Click(object sender, EventArgs e)
         {
-            if (_socket == null)
+            if (tcpSocket == null)
             {
                 try
                 {
-                    _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                    _socket.Bind(new IPEndPoint(IPAddress.Any, 23));
+                    tcpSocket = new TcpClient("192.168.0.123", 23);
                     ConnectButton.Text = "Disconnect";
-                    LEDcheckBox.Checked = true;
-                    LEDcheckBox.Invalidate();
-                    SendCommand("B1\r");
                 }
-                catch (Exception ex)
-                {
-                    _socket = null;
+                catch (Exception ex)              {
                     ConnectButton.Text = "Connect";
                     System.Console.WriteLine(ex.Message);
-                    string error_text = "Unable to open socket";
+                    string error_text = "Unable to open connection";
                     MessageBox.Show(error_text);
                 }
             }
             else
             {
-                _socket.Close();
-                _socket = null;
+                tcpSocket.Close();
+                tcpSocket = null;
                 ConnectButton.Text = "Connect";
-                LEDcheckBox.Checked = false;
-                LEDcheckBox.Invalidate();
-            }
-        }
-
-        delegate void SetTextCallback(string text);
-
-        private void AnalogValText(string text)
-        {
-            // InvokeRequired required compares the thread ID of the
-            // calling thread to the thread ID of the creating thread.
-            // If these threads are different, it returns true.
-            if (this.AnalogVal.InvokeRequired)
-            {
-                SetTextCallback d = new SetTextCallback(AnalogValText);
-                this.Invoke(d, new object[] { text });
-            }
-            else
-            {
-                this.AnalogVal.Text = text;
-            }
-        }
-
-        private void DataReceivedParser(string message)
-        {
-            char ch = message[0];
-            string param = message.Substring(1);
-            int numval = 0;
-            try
-            {   numval = Convert.ToInt16(param);}
-            catch (FormatException e)
-            { numval = 0; }
-            catch (OverflowException e)
-            { numval = 65535; }
-
-            switch (Char.ToUpper(ch))
-            {
-                case 'A':
-                    AnalogValText(numval.ToString());
-                    break;
-                case 'B':
-                    break;
-            }
-        }
-
-        string Arduino_reply;
-
-        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            SerialPort serial_port = (SerialPort)sender;
-            string indata = serial_port.ReadExisting();
-            foreach (char ch in indata)
-            {
-                if (ch=='\r'||ch=='\n')
-                {
-                    if (Arduino_reply.Length>0)
-                    {
-                        DataReceivedParser(Arduino_reply);
-                        Console.WriteLine(Arduino_reply);
-                        Arduino_reply = String.Empty;
-                        break;
-                    }
-                }
-                else
-                {
-                    Arduino_reply += ch;
-                }
             }
         }
 
         void SendCommand(string text)
         {
-            if (_socket != null)
+            if (tcpSocket.Connected)
             {
-//                _socket.Send(text);
+                byte[] Buffer = Encoding.ASCII.GetBytes(text);
+                Int32 Count = Encoding.ASCII.GetByteCount(text);
+                tcpSocket.GetStream().WriteAsync(Buffer, 0, Count);
             }
         }
 
-        private void AnalogRead_Click(object sender, EventArgs e)
+        private void W_Click(object sender, EventArgs e)
         {
-            SendCommand("A\r");
+            SendCommand("W\r");
         }
 
-        private void LEDcheckBox_CheckedChanged(object sender, EventArgs e)
+        private void S_Click(object sender, EventArgs e)
         {
-            if (LEDcheckBox.Checked)
-                SendCommand("B1\r");
-            else
-                SendCommand("B0\r");
+            SendCommand("S\r");
         }
     }
 }
